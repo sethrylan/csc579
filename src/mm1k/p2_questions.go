ee44811c369d2c91be10678606f16518b3386d4d
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2017-03-19 15:22:43 -0400

    Add confidence interval calcs.

--- src/mm1k/p2_questions.go
+++ src/mm1k/p2_questions.go
@@ -2,6 +2,10 @@ package mm1k
 
 import "fmt"
 
+var µ = 1.0
+var discard = 1000
+var replications = 30
+
 // P2Question1 : Let µ = 1 as before, the size of the queue K = 40, and the number
 // of customers served before a simulation run terminates C = 100,000. Plot the
 // average customer waiting time against the value of ρ, for ρ = 0.05 to
@@ -14,15 +18,30 @@ func P2Question1(seed int64) {
 	K := 40
 	C := 100000
 	for ρ := 0.05; ρ <= 0.95; ρ += 0.10 {
-		fmt.Printf("FIFO %f, %d, %d | ", ρ, K, C)
-		completes, _ := Simulate(ρ, 1.0, NewFIFO(K), C, seed)
-		fmt.Printf("W̄ = %.3f\n", Mean(completes, Wait))
 
-		fmt.Printf("LIFO %f, %d, %d | ", ρ, K, C)
-		completes, _ = Simulate(ρ, 1.0, NewLIFO(K), C, seed)
-		fmt.Printf("W̄ = %.3f\n", Mean(completes, Wait))
+		for _, queue := range []Queue{NewFIFO(K), NewLIFO(K), NewSJF(K)} {
+			waitTimes := make([]float64, replications)
+			fmt.Printf("% 10s %f, %d, %d | ", getType(queue), ρ, K, C)
+			for i := 0; i < replications; i++ {
+				fmt.Printf(".")
+				completes, _ := Simulate(ρ, µ, queue, C, seed+int64(i))
+				completes = removeFirstNByDeparture(completes, discard)
+				waitTimes[i] = Mean(completes, Wait)
+			}
+			sampleMean := MeanFloats(waitTimes)
+			sampleStdDev := StdDev(waitTimes, sampleMean)
+			fmt.Printf("W̄ = %.3f ±%.3f@95%%\n", sampleMean, sampleStdDev*2)
+		}
 
-		// TODO: SJF
+		//
+		// fmt.Printf("LIFO %f, %d, %d | ", ρ, K, C)
+		// completes, _ = Simulate(ρ, 1.0, NewLIFO(K), C, seed)
+		// fmt.Printf("W̄ = %.3f\n", Mean(completes, Wait))
+		//
+		// fmt.Printf("SJF  %f, %d, %d | ", ρ, K, C)
+		// completes, _ = Simulate(ρ, 1.0, NewSJF(K), C, seed)
+		// fmt.Printf("W̄ = %.3f\n", Mean(completes, Wait))
+
 		// TODO: ...
 	}
 }
