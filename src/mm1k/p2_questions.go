7b57f62e0446154ffe4c604e4c34d351a4c2177d
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2017-03-19 20:46:16 -0400

    Add concurrent implementation.

--- src/mm1k/p2_questions.go
+++ src/mm1k/p2_questions.go
@@ -1,10 +1,43 @@
 package mm1k
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+	"sync"
+)
 
 var µ = 1.0
 var discard = 1000
 var replications = 30
+
+func getFunctionName(i interface{}) string {
+	name := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+	return name[strings.LastIndex(name, ".")+1:]
+}
+
+func fifo(K int) Queue {
+	return NewFIFO(K)
+}
+
+func lifo(K int) Queue {
+	return NewLIFO(K)
+}
+
+func sjf(K int) Queue {
+	return NewSJF(K)
+}
+
+func prioNP(K int) Queue {
+	return NewPriority(K, true)
+}
+
+func prioP(K int) Queue {
+	return NewPriority(K, true)
+}
+
+// NewFIFO(K), NewLIFO(K), NewSJF(K), NewPriority(K, true)
 
 // P2Question1 : Let µ = 1 as before, the size of the queue K = 40, and the number
 // of customers served before a simulation run terminates C = 100,000. Plot the
@@ -19,29 +52,36 @@ func P2Question1(seed int64) {
 	C := 100000
 	for ρ := 0.05; ρ <= 0.95; ρ += 0.10 {
 
-		for _, queue := range []Queue{NewFIFO(K), NewLIFO(K), NewSJF(K)} {
-			waitTimes := make([]float64, replications)
-			fmt.Printf("% 10s %f, %d, %d | ", getType(queue), ρ, K, C)
+		for _, makerFunc := range []func(int) Queue{fifo, lifo, sjf, prioNP} {
+			var waitTimes []float64
+			fmt.Printf("% 10s %f, %d, %d | ", getFunctionName(makerFunc), ρ, K, C)
+			waitTimesChannel := make(chan float64, replications)
+
+			var wg sync.WaitGroup
 			for i := 0; i < replications; i++ {
-				fmt.Printf(".")
-				completes, _ := Simulate(ρ, µ, queue, C, seed+int64(i))
-				completes = RemoveFirstNByDeparture(completes, discard)
-				waitTimes[i] = Mean(completes, Wait)
+				wg.Add(1)
+				queue := makerFunc(K)
+				go replication(&wg, i, ρ, µ, queue, C, seed, waitTimesChannel)
 			}
+			wg.Wait()
+			close(waitTimesChannel)
+
+			for elem := range waitTimesChannel {
+				waitTimes = append(waitTimes, elem)
+			}
+
 			sampleMean := MeanFloats(waitTimes)
 			sampleStdDev := StdDev(waitTimes, sampleMean)
-			fmt.Printf("W̄ = %.3f ±%.3f@95%%\n", sampleMean, sampleStdDev*2)
+			fmt.Printf("  W̄ = %.3f ±%.3f@95%%\n", sampleMean, sampleStdDev*2)
 		}
-
-		//
-		// fmt.Printf("LIFO %f, %d, %d | ", ρ, K, C)
-		// completes, _ = Simulate(ρ, 1.0, NewLIFO(K), C, seed)
-		// fmt.Printf("W̄ = %.3f\n", Mean(completes, Wait))
-		//
-		// fmt.Printf("SJF  %f, %d, %d | ", ρ, K, C)
-		// completes, _ = Simulate(ρ, 1.0, NewSJF(K), C, seed)
-		// fmt.Printf("W̄ = %.3f\n", Mean(completes, Wait))
-
-		// TODO: ...
 	}
+}
+
+func replication(wg *sync.WaitGroup, i int, ρ float64, µ float64, queue Queue, C int, seed int64, ch chan<- float64) {
+	defer wg.Done()
+	defer fmt.Printf(".")
+	completes, _ := Simulate(ρ, µ, queue, C, seed+int64(i))
+	completes = RemoveFirstNByDeparture(completes, discard)
+	ch <- Mean(completes, Wait)
+	return
 }
