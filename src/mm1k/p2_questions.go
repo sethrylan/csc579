1b384df7c5366ded905afa3d56bf2cdabf08bab4
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2017-03-20 22:46:30 -0400

    Add worker concurrency and multiple queue calculations.

--- src/mm1k/p2_questions.go
+++ src/mm1k/p2_questions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"runtime"
+	"sort"
 	"strings"
 	"sync"
 )
@@ -11,6 +12,11 @@ import (
 var µ = 1.0
 var discard = 1000
 var replications = 30
+
+type queueTime struct {
+	queue int
+	time  float64
+}
 
 func getFunctionName(i interface{}) string {
 	name := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
@@ -37,6 +43,8 @@ func prioP(K int) Queue {
 	return NewPriority(K, true)
 }
 
+type queueTimeMap map[int][]float64
+
 // NewFIFO(K), NewLIFO(K), NewSJF(K), NewPriority(K, true)
 
 // P2Question1 : Let µ = 1 as before, the size of the queue K = 40, and the number
@@ -53,35 +61,62 @@ func P2Question1(seed int64) {
 	for ρ := 0.05; ρ <= 0.95; ρ += 0.10 {
 
 		for _, makerFunc := range []func(int) Queue{fifo, lifo, sjf, prioNP} {
-			var waitTimes []float64
+			var averageWaitTimes = make(queueTimeMap)
 			fmt.Printf("% 10s %f, %d, %d | ", getFunctionName(makerFunc), ρ, K, C)
-			waitTimesChannel := make(chan float64, replications)
+			averageWaitTimesChannel := make(chan []float64, replications)
 
 			var wg sync.WaitGroup
 			for i := 0; i < replications; i++ {
 				wg.Add(1)
 				queue := makerFunc(K)
-				go replication(&wg, i, ρ, µ, queue, C, seed, waitTimesChannel)
+				go replication(&wg, i, ρ, µ, queue, C, seed, averageWaitTimesChannel)
 			}
 			wg.Wait()
-			close(waitTimesChannel)
+			close(averageWaitTimesChannel)
 
-			for elem := range waitTimesChannel {
-				waitTimes = append(waitTimes, elem)
+			for waitTimesArray := range averageWaitTimesChannel {
+				for queueIndex, w := range waitTimesArray {
+					averageWaitTimes[queueIndex] = append(averageWaitTimes[queueIndex], w)
+				}
 			}
 
-			sampleMean := MeanFloats(waitTimes)
-			sampleStdDev := StdDev(waitTimes, sampleMean)
-			fmt.Printf("  W̄ = %.3f ±%.3f@95%%\n", sampleMean, sampleStdDev*2)
+			var keys []int
+			for k := range averageWaitTimes {
+				keys = append(keys, k)
+			}
+			sort.Ints(keys)
+			for _, k := range keys {
+				sampleMean := MeanFloats(averageWaitTimes[k])
+				sampleStdDev := StdDev(averageWaitTimes[k], sampleMean)
+				fmt.Printf("  W̄%d = %.3f ±%.3f@95%%", k, sampleMean, sampleStdDev*2)
+			}
+			fmt.Println()
+			// } else {
+			//  sampleMean := MeanFloats(waitTimes[0])
+			//  sampleStdDev := StdDev(waitTimes[0], sampleMean)
+			//  fmt.Printf("  W̄ = %.3f ±%.3f@95%%\n", sampleMean, sampleStdDev*2)
+			// }
 		}
 	}
 }
 
-func replication(wg *sync.WaitGroup, i int, ρ float64, µ float64, queue Queue, C int, seed int64, ch chan<- float64) {
+// Yield the average wait time per queue (after discard)
+func replication(wg *sync.WaitGroup, i int, ρ float64, µ float64, queue Queue, C int, seed int64, ch chan<- []float64) {
 	defer wg.Done()
 	defer fmt.Printf(".")
 	completes, _ := Simulate(ρ, µ, queue, C, seed+int64(i))
 	completes = RemoveFirstNByDeparture(completes, discard)
-	ch <- Mean(completes, Wait)
+
+	m := make(map[int][]Customer)
+	for _, c := range completes {
+		m[c.PriorityQueue] = append(m[c.PriorityQueue], c)
+	}
+
+	averageWaitTimes := make([]float64, len(m))
+	for k := range m {
+		averageWaitTimes[k] = Mean(m[k], Wait)
+		// fmt.Printf("key[%s] value[%s]\n", k, m[k])
+	}
+	ch <- averageWaitTimes
 	return
 }
