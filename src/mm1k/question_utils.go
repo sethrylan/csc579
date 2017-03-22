bd4478bca235fafb28d25f31b708643a87a1cd77
Seth Rylan Gainey <seth.gainey@accenturefederal.com>
2017-03-21 23:21:05 -0400

    Add better duration formating.

--- src/mm1k/question_utils.go
+++ src/mm1k/question_utils.go
@@ -94,7 +94,7 @@ func RemoveFirstNByDeparture(customers []Customer, n int) []Customer {
 
 func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
-	fmt.Printf("%s took %s", name, elapsed)
+	fmt.Printf("%s took %.3f", name, elapsed.Seconds())
 }
 
 func getType(myvar interface{}) (s string) {
